25697bc4a03466ddb0973e4f77e43e571fed53f5
Angga Pradana <saya.angga@hotmail.com>
2023-01-20 10:55:19 +0700

    update cara menampilkan data fungsi getperson

--- apimethod.php
+++ apimethod.php
@@ -19,8 +19,8 @@ class Person
 							'data' => $data
 						);
 		header('Content-Type: application/json');
-		//echo json_encode($response);
-		return json_encode($response);
+		echo json_encode($response);
+		//return json_encode($response);
 	}
 
 	public function get_person_by_id($id=0)
